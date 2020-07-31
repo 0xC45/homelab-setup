af3e7f5286bbbf8428080cd7291b2ac7f4cfda7e
Jason Vigil <jason@0xc45.com>
2020-07-30 21:03:17 -0600

    Improve Kubernetes "cleanup" script
    Apparently, `kubeadm reset` doesn't fully clean up remnants of any
    previous CNI. So, I added some commands to cleanup `/etc/cni/net.d` as
    well as reset all iptables entries. This is probably still not
    "foolproof" and may not work for fully cleaning up every CNI out there.

--- scripts/cleanup-k8s.bash
+++ scripts/cleanup-k8s.bash
@@ -1,8 +1,39 @@
 #!/usr/bin/env bash
 
+k8s_cleanup_cmd="kubeadm reset --force && "
+k8s_cleanup_cmd+="rm -rf /etc/cni/net.d && "
+k8s_cleanup_cmd+="iptables -P INPUT ACCEPT && "
+k8s_cleanup_cmd+="iptables -P FORWARD ACCEPT && "
+k8s_cleanup_cmd+="iptables -P OUTPUT ACCEPT && "
+k8s_cleanup_cmd+="iptables -t nat -F && "
+k8s_cleanup_cmd+="iptables -t mangle -F && "
+k8s_cleanup_cmd+="iptables -F && "
+k8s_cleanup_cmd+="iptables -X && "
+k8s_cleanup_cmd+="iptables-legacy -P INPUT ACCEPT && "
+k8s_cleanup_cmd+="iptables-legacy -P FORWARD ACCEPT && "
+k8s_cleanup_cmd+="iptables-legacy -P OUTPUT ACCEPT && "
+k8s_cleanup_cmd+="iptables-legacy -t nat -F && "
+k8s_cleanup_cmd+="iptables-legacy -t mangle -F && "
+k8s_cleanup_cmd+="iptables-legacy -F && "
+k8s_cleanup_cmd+="iptables-legacy -X && "
+k8s_cleanup_cmd+="ip6tables -P INPUT ACCEPT && "
+k8s_cleanup_cmd+="ip6tables -P FORWARD ACCEPT && "
+k8s_cleanup_cmd+="ip6tables -P OUTPUT ACCEPT && "
+k8s_cleanup_cmd+="ip6tables -t nat -F && "
+k8s_cleanup_cmd+="ip6tables -t mangle -F && "
+k8s_cleanup_cmd+="ip6tables -F && "
+k8s_cleanup_cmd+="ip6tables -X && "
+k8s_cleanup_cmd+="ip6tables-legacy -P INPUT ACCEPT && "
+k8s_cleanup_cmd+="ip6tables-legacy -P FORWARD ACCEPT && "
+k8s_cleanup_cmd+="ip6tables-legacy -P OUTPUT ACCEPT && "
+k8s_cleanup_cmd+="ip6tables-legacy -t nat -F && "
+k8s_cleanup_cmd+="ip6tables-legacy -t mangle -F && "
+k8s_cleanup_cmd+="ip6tables-legacy -F && "
+k8s_cleanup_cmd+="ip6tables-legacy -X"
+
 ansible \
   k8s \
   -i hosts.yml \
   --ask-vault-pass \
   --become \
-  -a "/bin/bash -c 'kubeadm reset --force'"
+  -a "/bin/bash -c '${k8s_cleanup_cmd}'"
